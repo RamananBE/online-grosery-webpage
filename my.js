fe4fb1913f4628af9a1c73c22d8f1569f25dd759
Ramanan <tramananbe@gmail.com>
2024-03-04 19:17:45 +0530

    Initial commit

---
+++ my.js
@@ -0,0 +1,6 @@
+
+        // Add JavaScript to handle the button click event
+        document.getElementById("nextButton").addEventListener("click", function() {
+            // Redirect to the next page when the button is clicked
+            window.location.href = "second_page.html";
+        });
